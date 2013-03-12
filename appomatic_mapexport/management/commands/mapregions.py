47a827007f7c9a4d570f65c4c9d4e5db1deac370
root <root@ew4.(none)>
2013-03-12 10:00:46 +0000

    Added a region export tool

---
+++ appomatic_mapexport/management/commands/mapregions.py
@@ -0,0 +1,54 @@
+import django.db
+import django.core.management.base
+import appomatic_mapdata.models
+import optparse
+import contextlib
+import shapely.wkb
+import shapely.wkt
+import fastkml.kml
+import fastkml.styles
+
+def dictreader(cur):
+    for row in cur:
+        yield dict(zip([col[0] for col in cur.description], row))
+
+class Command(django.core.management.base.BaseCommand):
+    help = 'Calculates and exports clusters of events'
+
+    def extract_kml(self):
+        kml = fastkml.kml.KML()
+        ns = '{http://www.opengis.net/kml/2.2}'
+        doc = fastkml.kml.Document(ns, 'regions', 'Regions', 'Region names and codes used by SkyTruth')
+        kml.append(doc)
+
+        self.cur.execute("""
+          select
+            src,
+            name,
+            code,
+            ST_AsText(the_geom) as the_geom
+          from
+            region
+        """)
+
+        for row in dictreader(self.cur):
+            keys = row.keys()
+            keys.sort()
+            placemark = fastkml.kml.Placemark('{http://www.opengis.net/kml/2.2}', "%(code)s" % row, "", "%(name)s (%(code)s, from %(src)s)" % row)
+            placemark.geometry = shapely.wkt.loads(str(row['the_geom']))
+            doc.append(placemark)
+
+        return kml.to_string(prettyprint=True)
+
+
+    def handle(self, filename, *args, **options):
+        try:
+            with contextlib.closing(django.db.connection.cursor()) as cur:
+                self.cur = cur
+
+                with open(filename, "w") as f:
+                    f.write(self.extract_kml().encode('utf-8'))
+        except Exception, e:
+            print e
+            import traceback
+            traceback.print_exc()
