3a38458ec3b2b985e4685e118222f045e88b10cd
Django Production environment <django@ewn3.skytruth.org>
2013-04-04 12:12:58 +0000

    More styling and more documents copied over

--- appomatic_alerts/__urls__.py
+++ appomatic_alerts/__urls__.py
@@ -1,10 +1,13 @@
 from django.conf.urls import *
 from django.conf import settings
+import django.views.generic
 
 urlpatterns = patterns('',
-    (r'^alerts/?$', 'appomatic_alerts.views.index'),
-    (r'^alerts/subscribe/?$', 'appomatic_alerts.views.subscribe'),
-    (r'^alerts/about/?$', 'appomatic_alerts.views.about'),
-    (r'^alerts/api/?$', 'appomatic_alerts.views.api'),
-    (r'^alerts/support/?$', 'appomatic_alerts.views.support'),
+    url(r'^alerts/?$', django.views.generic.TemplateView.as_view(template_name='appomatic_alerts/index.html'), name="appomatic_alerts_index"),
+    url(r'^alerts/subscribe/?$', django.views.generic.TemplateView.as_view(template_name='appomatic_alerts/subscribe.html'), name="appomatic_alerts_subscribe"),
+    url(r'^alerts/about/?$', django.views.generic.TemplateView.as_view(template_name='appomatic_alerts/about.html'), name="appomatic_alerts_about"),
+    url(r'^alerts/api/?$', django.views.generic.TemplateView.as_view(template_name='appomatic_alerts/api.html'), name="appomatic_alerts_api"),
+    url(r'^alerts/support/?$', django.views.generic.TemplateView.as_view(template_name='appomatic_alerts/support.html'), name="appomatic_alerts_support"),
+    url(r'^alerts/tags/?$', django.views.generic.TemplateView.as_view(template_name='appomatic_alerts/tags.html'), name="appomatic_alerts_tags"),
+    url(r'^alerts/sources/?$', django.views.generic.TemplateView.as_view(template_name='appomatic_alerts/sources.html'), name="appomatic_alerts_sources"),
 )
