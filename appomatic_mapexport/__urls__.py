c21f0649fb1199b2c055cc055355e7320b278e46
Django Production environment <django@ewn3.skytruth.org>
2013-04-24 15:05:47 +0000

    UI for map export#

--- appomatic_mapexport/__urls__.py
+++ appomatic_mapexport/__urls__.py
@@ -2,6 +2,7 @@ from django.conf.urls import *
 from django.conf import settings
 
 urlpatterns = patterns('',
+    (r'^mapexport/?$', 'appomatic_mapexport.views.index'),
     (r'^mapexport/kml/?$', 'appomatic_mapexport.views.kmlformat'),
     (r'^mapexport/csv/?$', 'appomatic_mapexport.views.csvformat'),
 )
