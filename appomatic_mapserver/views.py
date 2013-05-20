7e83f10902b40af9d5c8b998ee1715cede341a71
Egil Moeller <egil.moller@freecode.no>
2013-05-20 21:40:49 +0200

    Added support for builtin maps and layers

--- appomatic_mapserver/views.py
+++ appomatic_mapserver/views.py
@@ -86,9 +86,11 @@ def mapserver(request, application):
                           if datetimemin < kmldir < datetimemax]}
 
 def application(request, application):
+    urlquery = dict(request.GET.iteritems())
+    urlquery['application'] = application
     return django.shortcuts.render_to_response(
         'appomatic_mapserver/application.html',
-        {'application': appomatic_mapserver.models.Application.objects.get(slug=application)},
+        {'application': appomatic_mapserver.models.BaseApplication(urlquery)},
         context_instance=django.template.RequestContext(request))
 
 def index(request):
