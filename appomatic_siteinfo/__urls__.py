ca4eaee9c7394340adf759b6efa4b96479aaa0fe
Egil Moeller <egil.moller@freecode.no>
2013-06-25 11:12:45 +0200

    Better clustering

--- appomatic_siteinfo/__urls__.py
+++ appomatic_siteinfo/__urls__.py
@@ -5,5 +5,6 @@ urlpatterns = django.conf.urls.patterns('',
     django.conf.urls.url(r'^siteinfo/?$', django.views.generic.TemplateView.as_view(template_name='appomatic_siteinfo/index.html'), name="appomatic_siteinfo_index"),
     django.conf.urls.url(r'^siteinfo/search/?$', 'appomatic_siteinfo.views.search'),
     django.conf.urls.url(r'^siteinfo/all/(?P<model>.*)/?$', 'appomatic_siteinfo.views.basemodel'),
+    django.conf.urls.url(r'^siteinfo/clustersitelist/?$', 'appomatic_siteinfo.views.clustersitelist'),
     django.conf.urls.url(r'^siteinfo/(?P<guuid>.*)/?$', 'appomatic_siteinfo.views.basemodel'),
 )
