f5e9e03620132535203ff1fe08c1063fee4b7fa0
Egil Moeller <egil.moller@freecode.no>
2013-06-28 09:27:27 +0200

    Index object type

--- appomatic_siteinfo/search_indexes.py
+++ appomatic_siteinfo/search_indexes.py
@@ -5,28 +5,35 @@ import appomatic_siteinfo.models
 class CompanyIndex(haystack.indexes.ModelSearchIndex, haystack.indexes.Indexable):
     class Meta:
         model = appomatic_siteinfo.models.Company
+    model_type = haystack.indexes.CharField(model_attr='type_name')
 
 class SiteIndex(haystack.indexes.ModelSearchIndex, haystack.indexes.Indexable):
     class Meta:
         model = appomatic_siteinfo.models.Site
+    model_type = haystack.indexes.CharField(model_attr='type_name')
 
 class WellIndex(haystack.indexes.ModelSearchIndex, haystack.indexes.Indexable):
     class Meta:
         model = appomatic_siteinfo.models.Well
+    model_type = haystack.indexes.CharField(model_attr='type_name')
 
 class ChemicalPurposeIndex(haystack.indexes.ModelSearchIndex, haystack.indexes.Indexable):
     class Meta:
         model = appomatic_siteinfo.models.ChemicalPurpose
+    model_type = haystack.indexes.CharField(model_attr='type_name')
 
 class ChemicalPurposeIndex(haystack.indexes.ModelSearchIndex, haystack.indexes.Indexable):
     class Meta:
         model = appomatic_siteinfo.models.ChemicalPurpose
+    model_type = haystack.indexes.CharField(model_attr='type_name')
 
 class ChemicalIndex(haystack.indexes.ModelSearchIndex, haystack.indexes.Indexable):
     class Meta:
         model = appomatic_siteinfo.models.Chemical
+    model_type = haystack.indexes.CharField(model_attr='type_name')
 
 class EventIndex(haystack.indexes.ModelSearchIndex, haystack.indexes.Indexable):
     class Meta:
         model = appomatic_siteinfo.models.Event
+    model_type = haystack.indexes.CharField(model_attr='type_name')
 
