ef1b2835185830306139e931e2a4a3cbc330f64e
Egil Moeller <egil.moller@freecode.no>
2013-08-27 15:18:45 +0200

    More fracbot stuff

--- appomatic_fracbotserver/__urls__.py
+++ appomatic_fracbotserver/__urls__.py
@@ -3,4 +3,5 @@ from django.conf import settings
 
 urlpatterns = patterns('',
     (r'^fracbot/check-records/?$', 'appomatic_fracbotserver.views.check_records'),
+    (r'^fracbot/parse-pdf/?$', 'appomatic_fracbotserver.views.parse_pdf'),
 )
