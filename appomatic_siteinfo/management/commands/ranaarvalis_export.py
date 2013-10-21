85006ea030cfa207a3389dbd427d4863800af93b
Egil Moeller <egil.moller@freecode.no>
2013-10-21 16:35:20 +0200

    Filter to only sites with activity in 2010

--- appomatic_siteinfo/management/commands/ranaarvalis_export.py
+++ appomatic_siteinfo/management/commands/ranaarvalis_export.py
@@ -26,7 +26,31 @@ class Command(django.core.management.base.BaseCommand):
     def handle2(self, *args, **kwargs):
         with contextlib.closing(django.db.connection.cursor()) as cur:
             cur.execute("""
-select latitude y, longitude x from appomatic_siteinfo_site s join appomatic_siteinfo_locationdata l on s.locationdata_ptr_id = l.basemodel_ptr_id join appomatic_siteinfo_basemodel b on l.basemodel_ptr_id = b.id join region r on st_contains(r.the_geom, l.location) and r.name = 'Tioga County, PA'
+              select
+                latitude y, longitude x
+              from
+                appomatic_siteinfo_site s
+                join appomatic_siteinfo_locationdata l on
+                  s.locationdata_ptr_id = l.basemodel_ptr_id
+                join appomatic_siteinfo_basemodel b on
+                  l.basemodel_ptr_id = b.id
+                join region r on
+                  st_contains(r.the_geom, l.location)
+                  and r.name = 'Tioga County, PA'
+                join (
+                  select distinct
+                    se.site_id
+                  from
+                    appomatic_siteinfo_event e
+                    join appomatic_siteinfo_siteevent se on
+                      e.locationdata_ptr_id = se.event_ptr_id
+                      and e.datetime < '2011-01-01'
+                    join appomatic_siteinfo_statusevent ste on
+                      se.event_ptr_id = ste.siteevent_ptr_id
+                    join appomatic_siteinfo_status st on
+                      ste.status_id = st.basemodel_ptr_id
+                      and st.name in ('empty', 'equipment')) h on
+                  s.locationdata_ptr_id = h.site_id
             """)
 
             for site in fcdjangoutils.sqlutils.dictreader(cur):
