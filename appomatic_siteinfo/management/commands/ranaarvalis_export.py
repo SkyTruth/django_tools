ae54f9d3c32e443e3b461627eba429aef87da39b
Egil Moeller <egil.moller@freecode.no>
2013-10-31 15:20:29 +0100

    More generic export

--- appomatic_siteinfo/management/commands/ranaarvalis_export.py
+++ appomatic_siteinfo/management/commands/ranaarvalis_export.py
@@ -1,5 +1,7 @@
+import optparse
 import fcdjangoutils.sqlutils
 import json
+import geojson
 import contextlib
 import pyproj
 import shapely.wkt
@@ -14,6 +16,24 @@ geod = pyproj.Geod(ellps="WGS84")
 
 class Command(django.core.management.base.BaseCommand):
 
+    option_list = django.core.management.base.BaseCommand.option_list + (
+        optparse.make_option('--year',
+            action='store',
+            default="2010",
+            help='Region to filter to. Should be a value from the "code" column of the region table'),
+        optparse.make_option('--region',
+            action='store',
+            default="US-PA",
+            help='Region to filter to. Should be a value from the "code" column of the region table'),
+        optparse.make_option('--format',
+            action='store',
+            default="pybossa",
+            help='Output format: pybossa, geojson'),
+        )
+
+    layers = {"2010": {"url": "https://mapsengine.google.com/06136759344167181854-15360160187865123339-4/wms/",
+                       "options": {"version": "1.3.0", "layers":"06136759344167181854-16779344390631852423-4"}}}
+
     @django.db.transaction.commit_manually
     def handle(self, *args, **kwargs):
         try:
@@ -27,7 +47,11 @@ class Command(django.core.management.base.BaseCommand):
         with contextlib.closing(django.db.connection.cursor()) as cur:
             cur.execute("""
               select
-                latitude y, longitude x
+                l.latitude y,
+                l.longitude x,
+                b.guuid as guuid,
+                c.name as county,
+                substring(st.name from 3) as state
               from
                 appomatic_siteinfo_site s
                 join appomatic_siteinfo_locationdata l on
@@ -36,7 +60,13 @@ class Command(django.core.management.base.BaseCommand):
                   l.basemodel_ptr_id = b.id
                 join region r on
                   st_contains(r.the_geom, l.location)
-                  and r.name = 'Tioga County, PA'
+                  and r.code = %(region)s
+                join region c on
+                  st_contains(c.the_geom, l.location)
+                  and c.src = 'US-COUNTY'
+                join region st on
+                  st_contains(st.the_geom, l.location)
+                  and st.src = 'US-STATE'
                 join (
                   select distinct
                     se.site_id
@@ -44,24 +74,51 @@ class Command(django.core.management.base.BaseCommand):
                     appomatic_siteinfo_event e
                     join appomatic_siteinfo_siteevent se on
                       e.locationdata_ptr_id = se.event_ptr_id
-                      and e.datetime < '2011-01-01'
+                      and e.datetime < (%(year)s || '-12-29')::timestamp
                     join appomatic_siteinfo_statusevent ste on
                       se.event_ptr_id = ste.siteevent_ptr_id
                     join appomatic_siteinfo_status st on
                       ste.status_id = st.basemodel_ptr_id
                       and st.name in ('empty', 'equipment')) h on
                   s.locationdata_ptr_id = h.site_id
-            """)
+            """, kwargs)
 
+            if kwargs['format'] == 'geojson':
+                print '{"type": "FeatureCollection", "features": ['
+
+            first = True
             for site in fcdjangoutils.sqlutils.dictreader(cur):
 
-                dummy, top, dummy = geod.fwd(site['x'], site['y'], 0, 750)
-                left, dummy, dummy = geod.fwd(site['x'], site['y'], 270, 750)
-                right, dummy, dummy = geod.fwd(site['x'], site['y'], 90, 750)
-                dummy, bottom, dummy = geod.fwd(site['x'], site['y'], 180, 750)
+                dummy, top, dummy = geod.fwd(site['x'], site['y'], 0, 375)
+                left, dummy, dummy = geod.fwd(site['x'], site['y'], 270, 375)
+                right, dummy, dummy = geod.fwd(site['x'], site['y'], 90, 375)
+                dummy, bottom, dummy = geod.fwd(site['x'], site['y'], 180, 375)
                 
-                print json.dumps({
-                        "url": "https://mapsengine.google.com/06136759344167181854-15360160187865123339-4/wms/",
-                        "options": {"version": "1.3.0", "layers":"06136759344167181854-16779344390631852423-4"},
-                        "bbox": "%s,%s,%s,%s" % (left, bottom, right, top)
-                        })
+                info = {
+                    "latitude": site['y'],
+                    "longitude": site['x'],
+                    "SiteID": site["guuid"],
+                    "county": site["county"],
+                    "state": site["state"],
+                    "year": kwargs["year"]
+                    }
+
+                if kwargs['format'] == 'pybossa':
+                    info.update(self.layers[kwargs["year"]])
+                    info.update({"bbox": "%s,%s,%s,%s" % (left, bottom, right, top)})
+                    print json.dumps(info)
+                elif kwargs['format'] == 'geojson':
+                    if not first:
+                        print ","
+                    print geojson.dumps(
+                        geojson.Feature(
+                            geometry=geojson.Polygon(
+                                coordinates=[[[left, top], [right, top], [right, bottom], [left, bottom], [left, top]]]),
+                            properties=info
+                            )
+                        )
+                    
+                first = False
+
+            if kwargs['format'] == 'geojson':
+                print ']}'
